b2782bbc17b4ffa26dc8c58707e574719b63da37
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-10-18 23:45:48 +0530

    Files added

---
+++ Express-JS/index1.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.write("<h1>Welcome to the home page</h1>");
+    res.write("<h1>Welcome to the home page again</h1>");
+});
+
+app.get("/about", (req,res) => {
+    res.send("Welcome to the about page");
+});
+
+app.get("/contact", (req,res) => {
+    res.send("Welcome to the contact page");
+});
+
+
+
+app.listen(port, () => {
+    console.log("Listening to port 3000");
+});
