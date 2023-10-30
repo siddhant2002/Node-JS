507720eb950c5aa9483ace56416544b7e5c11a91
Siddhant2002 <siddhantswarupmallick@gmail.com>
2023-10-30 17:53:28 +0530

    Files added

---
+++ Express-JS/index.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+const express = require("express");
+
+const app = express();
+
+static_path = path.join(__dirname,"./public")
+
+console.log(static_path);
+
+app.use(express.static(static_path));
+
+app.get("/", (req,res) => {
+    res.send("Hello world from the thapa");
+});
+
+app.get("/blog", (req,res) => {
+    res.send("Hello from the blog page");
+})
+
+app.get("/about", (req,res) => {
+    res.send("Hello from the about page");
+});
+
+app.get("/contact", (req,res) => {
+    res.send("Hello from the contact page");
+});
+
+app.listen(3000, () => {
+    console.log("listening at port number 3000");
+});
