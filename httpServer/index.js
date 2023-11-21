f1ff1702adfcfbadf6723218b85471364147110c
Siddhant Swarup Mallick <78552027+siddhant2002@users.noreply.github.com>
2023-11-21 22:52:38 +0530

    Update index.js

--- httpServer/index.js
+++ httpServer/index.js
@@ -29,6 +29,7 @@ const server = http.createServer((req,res) => {
         res.end("404 error pages.");
     }
 });
+
 server.listen(8000, "127.0.0.1", () => {
     console.log("Listening to the port");
 });
