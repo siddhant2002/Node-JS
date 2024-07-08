cad5a5ffc73342c1a532a145a40346d57b206aa6
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-07-08 21:43:03 +0530

    Files updated

--- Js/script.js
+++ Js/script.js
@@ -1,11 +1,52 @@
-var x=10;
+// var x=10;
 
-console.log(x);
-function task()
+// console.log(x);
+// function task()
+// {
+//     var x=100;
+//     console.log(x);
+// }
+// a=7;
+// let p=100;
+// console.log(p);
+
+
+
+// Block scope
+
+// let b=100;
+
+// {
+//     let b=1;
+//     const c=5;
+//     var a=10;
+//     console.log(a);
+//     console.log(b);
+//     console.log(c);
+// }
+// console.log(b);
+// console.log(a);
+
+
+// Closures
+
+function z()
 {
-    var x=100;
-    console.log(x);
+    var a=10;
+    function y()
+    {
+        console.log(a);
+        var a=5;
+        function x()
+        {
+            console.log(a);
+            return x;
+        }
+        x();
+        return y;
+    }
+    y();
 }
-a=7;
-let p=100;
-console.log(p);
+var data = z();
+
+print(z);
