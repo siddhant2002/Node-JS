98cd0616b8bb3b7667d4e47e5cdb9dd41ee019e3
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-07-12 21:52:42 +0530

    Files added

--- Js/script.js
+++ Js/script.js
@@ -30,23 +30,44 @@
 
 // Closures
 
-function z()
-{
-    var a=10;
-    function y()
-    {
-        console.log(a);
-        var a=5;
-        function x()
-        {
-            console.log(a);
-            return x;
-        }
-        x();
-        return y;
-    }
-    y();
-}
-var data = z();
+// function z()
+// {
+//     var a=10;
+//     function y()
+//     {
+//         console.log(a);
+//         var a=5;
+//         function x()
+//         {
+//             console.log(a);
+//             return x;
+//         }
+//         x();
+//         return y;
+//     }
+//     y();
+// }
+// var data = z();
 
-print(z);
+// print(z);
+
+function x()
+{
+    var i=1;
+    setTimeout(function () {
+        console.log("hello");
+    }, 1000)
+    console.log(1);
+    console.log(2);
+    console.log(3);
+    console.log(4);
+    console.log(5);
+    console.log(6);
+    console.log(7);
+    console.log(8);
+    console.log(9);
+    console.log(10);
+    console.log(11);
+}
+
+x();
