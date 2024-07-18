461fcc393923605cba3e45c4741a85d77e286cce
Siddhant2002 <siddhantswarupmallick@gmail.com>
2024-07-18 22:16:13 +0530

    Files Updated

--- Js/script.js
+++ Js/script.js
@@ -51,23 +51,44 @@
 
 // print(z);
 
-function x()
+// function x()
+// {
+//     var i=1;
+//     setTimeout(function () {
+//         console.log("hello");
+//     }, 1000)
+//     console.log(1);
+//     console.log(2);
+//     console.log(3);
+//     console.log(4);
+//     console.log(5);
+//     console.log(6);
+//     console.log(7);
+//     console.log(8);
+//     console.log(9);
+//     console.log(10);
+//     console.log(11);
+// }
+
+// x();
+
+function a()
 {
-    var i=1;
-    setTimeout(function () {
-        console.log("hello");
-    }, 1000)
-    console.log(1);
-    console.log(2);
-    console.log(3);
-    console.log(4);
-    console.log(5);
-    console.log(6);
-    console.log(7);
-    console.log(8);
-    console.log(9);
-    console.log(10);
-    console.log(11);
+    console.log("Hi");
 }
 
-x();
+var b = function ()
+{
+    console.log("Helio");
+}
+
+var c = function xyz()
+{
+    console.log("Heliiiooo");
+    // xyz();
+}
+
+a();
+b();
+c();
+// xyz();  Error
